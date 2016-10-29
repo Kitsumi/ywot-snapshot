84569c781ea71becef6ad394223ae92661384a69
Kitsumi <jaycewebb@gmail.com>
2016-10-29 09:10:38 -0600

    Update client.js

--- client.js
+++ client.js
@@ -17,7 +17,7 @@ socket.addEventListener('open', function() {
     socket.onmessage = function(data) {
         var parsed = JSON.parse(data.data);
         if (parsed.kind == "tileUpdate") {
-            tilesToPicture(parsed, WorldProps.xsize, WorldProps.ysize);
+            //tilesToPicture(parsed, WorldProps.xsize, WorldProps.ysize);
         } else if (parsed.kind == "fetch") {
             tilesToPicture(parsed, WorldProps.xsize, WorldProps.ysize);
             console.log(`Got Tiles: ${recievedTiles}`);
